b63951e6a95e6a1a453f560b5f9b9416609ca606
kimnamlhn <kimnam.cpc@gmail.com>
2021-08-09 22:12:14 +0700

    validate email

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,29 @@
-//function to check mail
+let btn = document.querySelector(".btn");
+let email = document.querySelector("input[type=text]");
+let errP = document.querySelector("#error");
+let errIcon = document.querySelector("#error-icon");
 
+//function to check mail
+btn.addEventListener("click", (e) => {
+	e.preventDefault();
+	if (
+		/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+			email.value
+		)
+	) {
+		email.classList.remove("error");
+		errIcon.classList.remove("error");
+		errP.classList.remove("error");
+		return true;
+	} else {
+		email.classList.add("error");
+		errIcon.classList.add("error");
+		errP.classList.add("error");
+		setTimeout(() => {
+			email.classList.remove("error");
+			errIcon.classList.remove("error");
+			errP.classList.remove("error");
+		}, 3000);
+		return false;
+	}
+});
